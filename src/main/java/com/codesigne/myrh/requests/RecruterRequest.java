8cae7bce28cb0812dd8cda860048f06ed4fe3a6a
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2022-12-27 14:08:39 +0100

    create recruter request class

---
+++ src/main/java/com/codesigne/myrh/requests/RecruterRequest.java
@@ -0,0 +1,11 @@
+package com.codesigne.myrh.requests;
+
+public class RecruterRequest {
+
+    private String login;
+    private String password;
+    private String adress;
+    private String email;
+    private String phone;
+
+}
