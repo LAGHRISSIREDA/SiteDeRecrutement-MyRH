3f97800a01ff848abb254d8404da82d289549189
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2022-12-28 11:56:15 +0100

    add recruter Request Class

--- src/main/java/com/codesigne/myrh/requests/RecruterRequest.java
+++ src/main/java/com/codesigne/myrh/requests/RecruterRequest.java
@@ -2,10 +2,53 @@ package com.codesigne.myrh.requests;
 
 public class RecruterRequest {
 
-    private String login;
     private String password;
     private String adress;
     private String email;
     private String phone;
+    private String picture;
 
+    //getters and setters ------------------
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 }
