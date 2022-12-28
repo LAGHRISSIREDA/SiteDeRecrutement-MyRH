04d06db52610972361e3cc57d47c9a5087566dbc
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2022-12-28 11:55:49 +0100

    add recruter repository interface to manage data operations

---
+++ src/main/java/com/codesigne/myrh/repositories/RecruterRepository.java
@@ -0,0 +1,9 @@
+package com.codesigne.myrh.repositories;
+
+import com.codesigne.myrh.entities.RecruterEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecruterRepository extends CrudRepository<RecruterEntity,Long> {
+}
